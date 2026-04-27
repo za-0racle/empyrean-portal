aee14bf970e256d6beb913649029d8400bf18fc6
Oracletek <adedokunkhaleed@gmail.com>
2026-04-27 11:19:39 +0100

    Configure multi-page build for Netlify

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        signup: resolve(__dirname, 'signup.html'),
         catalog: resolve(__dirname, 'catalog.html'),
         dashboard: resolve(__dirname, 'dashboard.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        instructor: resolve(__dirname, 'instructor.html'),
+        // Add any other .html files you have here
       },
     },
   },
