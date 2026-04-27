cdd1d90698a3c431935c7636b0619dce98ca3d16
Oracletek <adedokunkhaleed@gmail.com>
2026-04-27 11:33:29 +0100

    Fix: Multi-page paths and build config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: './',
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
